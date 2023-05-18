c057eac865903be76d9b3d096d58d4e9b1aed16f
Giercko Contreras <110959684+gcontreraspalma@users.noreply.github.com>
2023-05-18 10:31:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const datosSucursales = {
 }
 
 // Ruta POST para recibir los parámetros y buscar el número de teléfono
-app.post('/buscar-telefono', (req, res) => {
+app.get('/buscar-telefono', (req, res) => {
   const { nombre, ubicacion, sucursal } = req.body;
   console.log(req.body)
   if (!nombre || !ubicacion ) {
